7715b762a0d022f59028b49e34404978d86c1e59
alex070202 <aleksandyr_bankov@abv.bg>
2025-05-21 17:10:37 +0300

    fix bug in email letter

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -12,8 +12,12 @@ urlpatterns = [
     
     # password reset
     path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='registration/password_reset_form.html'
+    template_name='registration/password_reset_form.html',
+    email_template_name='registration/password_reset_email.txt',            
+    html_email_template_name='registration/password_reset_email.html',      
+    subject_template_name='registration/password_reset_subject.txt',    
     ), name='password_reset'),
+
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='registration/password_reset_done.html'
     ), name='password_reset_done'),
